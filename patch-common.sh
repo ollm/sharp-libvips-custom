853c5d292bdd11e610f8433d8c68a3fe9fee9452
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2026-05-03 17:24:26 +0200

    upd: patch-common.sh

--- patch-common.sh
+++ patch-common.sh
@@ -10,18 +10,18 @@ find . \
   \( -path './.git' -o -path '*/.git/*' \) -prune -o \
   -type f \( -name '*.json' -o -name '*.js' \) \
   ! -name '*.bak' \
-  -exec sed -i.bak 's/@img\/sharp-/@img-custom\/sharp-/g' {} +
+  -exec sed -i.bak 's|@img/sharp-|@img-custom/sharp-|g' {} +
 
 # Replace lovell/sharp-libvips with ollm/sharp-libvips-custom in .json (For provenance)
 find . \
   \( -path './.git' -o -path '*/.git/*' \) -prune -o \
   -type f -name '*.json' \
   ! -name '*.bak' \
-  -exec sed -i.bak 's/lovell\/sharp-libvips/ollm\/sharp-libvips-custom/g' {} +
+  -exec sed -i.bak 's|lovell/sharp-libvips|ollm/sharp-libvips-custom|g' {} +
 
 # Replace version (Only for developing)
 find . \
   \( -path './.git' -o -path '*/.git/*' \) -prune -o \
   -type f -name '*.json' \
   ! -name '*.bak' \
-  -exec sed -i.bak 's/"1.3.0-rc.6"/"0.0.8"/g' {} +
+  -exec sed -i.bak 's|"1.3.0-rc.6"|"0.0.8"|g' {} +
