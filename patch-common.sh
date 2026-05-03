2be64c5ee831cd23e7286875bcb8637329f05047
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2026-05-03 16:27:32 +0200

    test 0.0.5

--- patch-common.sh
+++ patch-common.sh
@@ -24,4 +24,4 @@ find . \
   \( -path './.git' -o -path '*/.git/*' \) -prune -o \
   -type f -name '*.json' \
   ! -name '*.bak' \
-  -exec sed -i.bak 's/"1.3.0-rc.6"/"0.0.4"/g' {} +
+  -exec sed -i.bak 's/"1.3.0-rc.6"/"0.0.5"/g' {} +
