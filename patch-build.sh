07640f4e93e9083af5552e8cc200c0166ba489ba
Oleguer Llopart <oleguer.llopart.mora@gmail.com>
2026-05-10 10:29:58 +0200

    Fix Windows patching logic 3

--- patch-build.sh
+++ patch-build.sh
@@ -19,12 +19,12 @@ case ${PLATFORM} in
 
     # Switch from 'libvips/build-win64-mxe' to 'ollm/build-win64-mxe-custom'
     sed -i.bak 's|libvips/build-win64-mxe|ollm/build-win64-mxe-custom|' "$BUILD_FILE"
-    sed -i.bak "s|\${VERSION_VIPS}|${HASH}|g" "$BUILD_FILE"
-    sed -i.bak "s|\${VERSION_VIPS_SHORT}|${HASH}|g" "$BUILD_FILE"
+    sed -i.bak 's|${VERSION_VIPS}|${HASH}|g' "$BUILD_FILE"
+    sed -i.bak 's|${VERSION_VIPS_SHORT}|${HASH}|g' "$BUILD_FILE"
 
     # Temporarily, v${VERSION_VIPS} to v${VERSION_VIPS}-3
-    sed -i.bak 's|v\${VERSION_VIPS}|v${VERSION_VIPS}-3|' "$BUILD_FILE"
-    sed -i.bak 's|v${HASH}|v${VERSION_VIPS}-3|' "$BUILD_FILE"
+    sed -i.bak 's|v${VERSION_VIPS}|v${VERSION_VIPS}-3|g' "$BUILD_FILE"
+    sed -i.bak "s|v${HASH}|v${VERSION_VIPS}-3|" "$BUILD_FILE"
 
     rm -f "${BUILD_FILE}.bak"
 
